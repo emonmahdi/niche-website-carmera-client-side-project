aec2fbbaf1341d019e3e82eea931f9741975b40b
emonmahdi <developeremon1997@gmail.com>
2021-11-13 19:49:42 +0600

    product delete

--- src/Pages/Dashboard/ManageProduct/ManageProduct.js
+++ src/Pages/Dashboard/ManageProduct/ManageProduct.js
@@ -10,8 +10,23 @@ const ManageProduct = () => {
     }, [])
 
     // delete product
-     
+    const handleDeleteProduct = id => {
+        const procced = window.confirm('Are you sure you want to delete product?')
+        if(procced){
+            fetch(`https://floating-brushlands-69633.herokuapp.com/products/${id}`,{
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0){
+                    alert('Product Delete Successfully');
+                    const remaingProduct = allProduct.filter(product => product._id !== id)
+                    setAllProduct(remaingProduct);
+                }
+            })
 
+        }
+    }
     return (
         <div>
             <h2>Manage All Products Here.{allProduct.length} </h2>
@@ -27,7 +42,7 @@ const ManageProduct = () => {
                                         <p>{product.description}</p>
                                         <h5>Price: {product.price}</h5>
                                          
-                                        <button className='btn btn-warning mt-3'>Delete Product</button>
+                                        <button onClick={() => handleDeleteProduct(product._id)} className='btn btn-warning mt-3'>Delete</button>
                                         
                                         
                                      </div>
